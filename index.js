afafff69e6096933895a19f37854db4dd6ec8f93
zhangcx <zhangcx01@mail.jj.cn>
2019-12-08 13:26:20 +0800

    运行简单的检查，确保当前目录不是 Git 仓库

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const figlet = require('figlet');
 
 const files = require('./lib/files');
 
+// 先清空屏幕，然后展示一个banner
 clear();
 console.log(
     chalk.yellow(
         figlet.textSync('Ginit', { horizontalLayout: 'full' })
     )
 );
+
+// 运行简单的检查，确保当前目录不是 Git 仓库
+if(files.directoryExists('.git')) {
+    console.log(chalk.red('Already a git repository!'));
+    process.exit();
+}
