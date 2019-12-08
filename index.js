7352822ba9a3020012e97f71e046d6a1f0338abd
zhangcx <zhangcx01@mail.jj.cn>
2019-12-08 13:24:22 +0800

    先清空屏幕，然后展示一个banner

---
+++ index.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+const files = require('./lib/files');
+
+clear();
+console.log(
+    chalk.yellow(
+        figlet.textSync('Ginit', { horizontalLayout: 'full' })
+    )
+);
